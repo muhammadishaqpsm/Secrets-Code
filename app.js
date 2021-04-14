f49b1fa41902074e571242b9ae2ac97aedffb36f
muhammadishaqpsm <muhammadishaq.psm@gmail.com>
2021-04-14 16:19:27 +0500

    Add DOTENV File

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -20,8 +21,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-const secret = "This is our little secret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
